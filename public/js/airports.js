b460fae9170d76addf597812c4d7b3f10c33422d
Ryan Hill <me@ryanjhill.com>
2017-05-31 13:28:09 -0700

    Fixed incorrect read of json

--- public/js/airports.js
+++ public/js/airports.js
@@ -6,7 +6,7 @@ eqfeed_callback(
     "place": "Cape Town",
     "country": "South Africa",
     "iata": "CPT",
-    "coordinates": "[-33.9648017883,18.6016998291]",
+    "coordinates": [-33.9648017883,18.6016998291],
     "price": 1030,
     "date": "2017-07-01",
     "days_until": 1
@@ -16,7 +16,7 @@ eqfeed_callback(
     "place": "Lima",
     "country": "Peru",
     "iata": "LIM",
-    "coordinates": "[-12.021900177,-77.1143035889]",
+    "coordinates": [-12.021900177,-77.1143035889],
     "price": 1150,
     "date": "2017-07-01",
     "days_until": 1
@@ -26,7 +26,7 @@ eqfeed_callback(
     "place": "Buenos Aires",
     "country": "Argentina",
     "iata": "MDQ",
-    "coordinates": "[-37.9342,-57.5733]",
+    "coordinates": [-37.9342,-57.5733],
     "price": 1050,
     "date": "2017-07-01",
     "days_until": 1
@@ -36,7 +36,7 @@ eqfeed_callback(
     "place": "Haarlemmermeer, North Holland",
     "country": "The Netherlands",
     "iata": "AMS",
-    "coordinates": "[52.3086013794, 4.763889789579999]",
+    "coordinates": [52.3086013794, 4.763889789579999],
     "price": 890,
     "date": "2017-07-01",
     "days_until": 1
@@ -46,7 +46,7 @@ eqfeed_callback(
     "place": "Atlanta, Georgia",
     "country": "United States",
     "iata": "ATL",
-    "coordinates": "[33.63669967651367, -84.4281005859375]",
+    "coordinates": [33.63669967651367, -84.4281005859375],
     "price": 1300,
     "date": "2017-07-01",
     "days_until": 1
@@ -56,7 +56,7 @@ eqfeed_callback(
     "place": "El Prat, Catalonia",
     "country": "Spain",
     "iata": "BCN",
-    "coordinates": "[41.297100067139, 2.0784599781036]",
+    "coordinates": [41.297100067139, 2.0784599781036],
     "price": 600,
     "date": "2017-07-01",
     "days_until": 1
@@ -66,7 +66,7 @@ eqfeed_callback(
     "place": "Bang Phli, Samut Prakan",
     "country": "Thailand",
     "iata": "BKK",
-    "coordinates": "[13.681099891662598, 100.74700164794922]",
+    "coordinates": [13.681099891662598, 100.74700164794922],
     "price": 1000,
     "date": "2017-07-01",
     "days_until": 1
@@ -76,7 +76,7 @@ eqfeed_callback(
     "place": "Mumbai, Maharashtra",
     "country": "India",
     "iata": "BOM",
-    "coordinates": "[19.0886993408, 72.8678970337]",
+    "coordinates": [19.0886993408, 72.8678970337],
     "price": 1000,
     "date": "2017-07-01",
     "days_until": 1
@@ -86,7 +86,7 @@ eqfeed_callback(
     "place": "Baiyun-Huadu, Guangzhou, Guangdong",
     "country": "China",
     "iata": "CAN",
-    "coordinates": "[23.39240074157715, 113.29900360107422]",
+    "coordinates": [23.39240074157715, 113.29900360107422],
     "price": 1000,
     "date": "2017-07-01",
     "days_until": 1
@@ -96,7 +96,7 @@ eqfeed_callback(
     "place": "Roissy-en-France, Île-de-France",
     "country": "France",
     "iata": "CDG",
-    "coordinates": "[49.0127983093, 2.54999995232]",
+    "coordinates": [49.0127983093, 2.54999995232],
     "price": 1000,
     "date": "2017-07-05",
     "days_until": 5
@@ -106,7 +106,7 @@ eqfeed_callback(
     "place": "Tangerang, Banten",
     "country": "Indonesia",
     "iata": "CGK",
-    "coordinates": "[-6.1255698204,106.65599823]",
+    "coordinates": [-6.1255698204,106.65599823],
     "price": 780,
     "date": "2017-07-05",
     "days_until": 5
@@ -116,7 +116,7 @@ eqfeed_callback(
     "place": "Charlotte, North Carolina",
     "country": "United States",
     "iata": "CLT",
-    "coordinates": "[35.2140007019043, -80.94309997558594]",
+    "coordinates": [35.2140007019043, -80.94309997558594],
     "price": 300,
     "date": "2017-07-05",
     "days_until": 5
@@ -126,7 +126,7 @@ eqfeed_callback(
     "place": "Shuangliu-Wuhou, Chengdu, Sichuan",
     "country": "China",
     "iata": "CTU",
-    "coordinates": "[30.578500747680664, 103.9469985961914]",
+    "coordinates": [30.578500747680664, 103.9469985961914],
     "price": 900,
     "date": "2017-07-05",
     "days_until": 5
@@ -136,7 +136,7 @@ eqfeed_callback(
     "place": "Delhi",
     "country": "India",
     "iata": "DEL",
-    "coordinates": "[28.566499710083008, 77.10310363769531]",
+    "coordinates": [28.566499710083008, 77.10310363769531],
     "price": 1300,
     "date": "2017-07-05",
     "days_until": 5
@@ -146,7 +146,7 @@ eqfeed_callback(
     "place": "Denver, Colorado",
     "country": "United States",
     "iata": "DEN",
-    "coordinates": "[39.861698150635, -104.672996521]",
+    "coordinates": [39.861698150635, -104.672996521],
     "price": 200,
     "date": "2017-07-05",
     "days_until": 5
@@ -156,7 +156,7 @@ eqfeed_callback(
     "place": "Dallas-Fort Worth, Texas",
     "country": "United States",
     "iata": "DFW",
-    "coordinates": "[32.89680099487305, -97.03800201416016]",
+    "coordinates": [32.89680099487305, -97.03800201416016],
     "price": 850,
     "date": "2017-07-05",
     "days_until": 5
@@ -166,7 +166,7 @@ eqfeed_callback(
     "place": "Doha",
     "country": "Qatar",
     "iata": "DOH",
-    "coordinates": "[25.273056, 51.608056]",
+    "coordinates": [25.273056, 51.608056],
     "price": 1200,
     "date": "2017-07-05",
     "days_until": 5
@@ -176,7 +176,7 @@ eqfeed_callback(
     "place": "Garhoud, Dubai",
     "country": "United Arab Emirates",
     "iata": "DXB",
-    "coordinates": "[25.2527999878,55.3643989563]",
+    "coordinates": [25.2527999878,55.3643989563],
     "price": 1000,
     "date": "2017-07-05",
     "days_until": 5
@@ -186,7 +186,7 @@ eqfeed_callback(
     "place": "Newark, New Jersey",
     "country": "United States",
     "iata": "EWR",
-    "coordinates": "[40.692501068115234, -74.168701171875]",
+    "coordinates": [40.692501068115234, -74.168701171875],
     "price": 850,
     "date": "2017-07-05",
     "days_until": 5
@@ -196,7 +196,7 @@ eqfeed_callback(
     "place": "Fiumicino, Lazio",
     "country": "Italy",
     "iata": "FCO",
-    "coordinates": "[41.8002778, 12.2388889]",
+    "coordinates": [41.8002778, 12.2388889],
     "price": 1000,
     "date": "2017-07-05",
     "days_until": 5
@@ -206,7 +206,7 @@ eqfeed_callback(
     "place": "Frankfurt, Hesse",
     "country": "Germany",
     "iata": "FRA",
-    "coordinates": "[50.0333333,8.5705556]",
+    "coordinates": [50.0333333,8.5705556],
     "price": 850,
     "date": "2017-07-12",
     "days_until": 12
@@ -216,7 +216,7 @@ eqfeed_callback(
     "place": "Chek Lap Kok, Islands, Hong Kong",
     "country": "Hong Kong",
     "iata": "HKG",
-    "coordinates": "[22.3089008331,113.915000916]",
+    "coordinates": [22.3089008331,113.915000916],
     "price": 1000,
     "date": "2017-07-12",
     "days_until": 12
@@ -226,7 +226,7 @@ eqfeed_callback(
     "place": "Ōta, Tokyo",
     "country": "Japan",
     "iata": "HND",
-    "coordinates": "[35.552299,139.779999]",
+    "coordinates": [35.552299,139.779999],
     "price": 700,
     "date": "2017-07-12",
     "days_until": 12
@@ -236,7 +236,7 @@ eqfeed_callback(
     "place": "Houston, Texas",
     "country": "United States",
     "iata": "IAH",
-    "coordinates": "[29.984399795532227, -95.34140014648438]",
+    "coordinates": [29.984399795532227, -95.34140014648438],
     "price": 250,
     "date": "2017-07-12",
     "days_until": 12
@@ -246,7 +246,7 @@ eqfeed_callback(
     "place": "Incheon",
     "country": "Republic of Korea",
     "iata": "ICN",
-    "coordinates": "[37.46910095214844, 126.45099639892578]",
+    "coordinates": [37.46910095214844, 126.45099639892578],
     "price": 1000,
     "date": "2017-07-12",
     "days_until": 12
@@ -256,7 +256,7 @@ eqfeed_callback(
     "place": "Yeşilköy, Istanbul",
     "country": "Turkey",
     "iata": "IST",
-    "coordinates": "[40.9768981934, 28.814599990799998]",
+    "coordinates": [40.9768981934, 28.814599990799998],
     "price": 1300,
     "date": "2017-07-12",
     "days_until": 12
@@ -266,7 +266,7 @@ eqfeed_callback(
     "place": "Queens, New York City, New York",
     "country": "United States",
     "iata": "JFK",
-    "coordinates": "[40.63980103, -73.77890015]",
+    "coordinates": [40.63980103, -73.77890015],
     "price": 400,
     "date": "2017-07-12",
     "days_until": 12
@@ -276,7 +276,7 @@ eqfeed_callback(
     "place": "Guandu, Kunming, Yunnan",
     "country": "China",
     "iata": "KMG",
-    "coordinates": "[25.1019444,102.9291667]",
+    "coordinates": [25.1019444,102.9291667],
     "price": 1400,
     "date": "2017-07-12",
     "days_until": 12
@@ -286,7 +286,7 @@ eqfeed_callback(
     "place": "Sepang, Selangor",
     "country": "Malaysia",
     "iata": "KUL",
-    "coordinates": "[2.745579957962,101.70999908447]",
+    "coordinates": [2.745579957962,101.70999908447],
     "price": 1000,
     "date": "2017-07-12",
     "days_until": 12
@@ -296,7 +296,7 @@ eqfeed_callback(
     "place": "Las Vegas, Nevada",
     "country": "United States",
     "iata": "LAS",
-    "coordinates": "[36.08010101,-115.1520004]",
+    "coordinates": [36.08010101,-115.1520004],
     "price": 150,
     "date": "2017-07-12",
     "days_until": 12
@@ -306,7 +306,7 @@ eqfeed_callback(
     "place": "Los Angeles, California",
     "country": "United States",
     "iata": "LAX",
-    "coordinates": "[33.94250107,-118.4079971]",
+    "coordinates": [33.94250107,-118.4079971],
     "price": 100,
     "date": "2017-07-12",
     "days_until": 12
@@ -316,7 +316,7 @@ eqfeed_callback(
     "place": "Crawley, West Sussex",
     "country": "United Kingdom",
     "iata": "LGW",
-    "coordinates": "[51.148101806640625, -0.19027799367904663]",
+    "coordinates": [51.148101806640625, -0.19027799367904663],
     "price": 850,
     "date": "2017-07-12",
     "days_until": 12
@@ -326,7 +326,7 @@ eqfeed_callback(
     "place": "Hillingdon, London",
     "country": "United Kingdom",
     "iata": "LHR",
-    "coordinates": "[51.4706, -0.461941]",
+    "coordinates": [51.4706, -0.461941],
     "price": 1000,
     "date": "2017-07-21",
     "days_until": 21
@@ -336,7 +336,7 @@ eqfeed_callback(
     "place": "Barajas, Madrid",
     "country": "Spain",
     "iata": "MAD",
-    "coordinates": "[40.471926,-3.56264]",
+    "coordinates": [40.471926,-3.56264],
     "price": 1000,
     "date": "2017-07-21",
     "days_until": 21
@@ -346,7 +346,7 @@ eqfeed_callback(
     "place": "Orlando, Florida",
     "country": "United States",
     "iata": "MCO",
-    "coordinates": "[28.429399490356445, -81.30899810791016]",
+    "coordinates": [28.429399490356445, -81.30899810791016],
     "price": 400,
     "date": "2017-07-21",
     "days_until": 21
@@ -356,7 +356,7 @@ eqfeed_callback(
     "place": "Venustiano Carranza, Mexico City",
     "country": "Mexico",
     "iata": "MEX",
-    "coordinates": "[19.4363, -99.072098]",
+    "coordinates": [19.4363, -99.072098],
     "price": 1000,
     "date": "2017-07-21",
     "days_until": 21
@@ -366,7 +366,7 @@ eqfeed_callback(
     "place": "Miami-Dade County, Florida",
     "country": "United States",
     "iata": "MIA",
-    "coordinates": "[25.79319953918457,-80.29060363769531]",
+    "coordinates": [25.79319953918457,-80.29060363769531],
     "price": 300,
     "date": "2017-07-21",
     "days_until": 21
@@ -376,7 +376,7 @@ eqfeed_callback(
     "place": "Pasay/Parañaque, Metro Manila",
     "country": "Philippines",
     "iata": "MNL",
-    "coordinates": "[14.5086,121.019997]",
+    "coordinates": [14.5086,121.019997],
     "price": 1500,
     "date": "2017-07-21",
     "days_until": 21
@@ -386,7 +386,7 @@ eqfeed_callback(
     "place": "St. Paul, Minnesota",
     "country": "United States",
     "iata": "MSP",
-    "coordinates": "[44.881999969499994,-93.22180175780001]",
+    "coordinates": [44.881999969499994,-93.22180175780001],
     "price": 200,
     "date": "2017-07-21",
     "days_until": 21
@@ -396,7 +396,7 @@ eqfeed_callback(
     "place": "Freising, Bavaria",
     "country": "Germany",
     "iata": "MUC",
-    "coordinates": "[48.353801727295, 11.786100387573]",
+    "coordinates": [48.353801727295, 11.786100387573],
     "price": 350,
     "date": "2017-07-21",
     "days_until": 21
@@ -406,7 +406,7 @@ eqfeed_callback(
     "place": "Narita, Chiba",
     "country": "Japan",
     "iata": "NRT",
-    "coordinates": "[35.7647018433, 140.386001587]",
+    "coordinates": [35.7647018433, 140.386001587],
     "price": 800,
     "date": "2017-07-21",
     "days_until": 21
@@ -416,7 +416,7 @@ eqfeed_callback(
     "place": "Chicago, Illinois",
     "country": "United States",
     "iata": "ORD",
-    "coordinates": "[41.97859955,-87.90480042]",
+    "coordinates": [41.97859955,-87.90480042],
     "price": 200,
     "date": "2017-08-06",
     "days_until": 70
@@ -426,7 +426,7 @@ eqfeed_callback(
     "place": "Chaoyang-Shunyi, Beijing",
     "country": "China",
     "iata": "PEK",
-    "coordinates": "[40.080101013183594, 116.58499908447266]",
+    "coordinates": [40.080101013183594, 116.58499908447266],
     "price": 1000,
     "date": "2017-08-06",
     "days_until": 70
@@ -436,7 +436,7 @@ eqfeed_callback(
     "place": "Phoenix, Arizona",
     "country": "United States",
     "iata": "PHX",
-    "coordinates": "[33.43429946899414, -112.01200103759766]",
+    "coordinates": [33.43429946899414, -112.01200103759766],
     "price": 200,
     "date": "2017-08-06",
     "days_until": 70
@@ -446,7 +446,7 @@ eqfeed_callback(
     "place": "Pudong, Shanghai",
     "country": "China",
     "iata": "PVG",
-    "coordinates": "[31.143400192260742,121.80500030517578]",
+    "coordinates": [31.143400192260742,121.80500030517578],
     "price": 1800,
     "date": "2017-08-06",
     "days_until": 70
@@ -456,7 +456,7 @@ eqfeed_callback(
     "place": "SeaTac, Washington",
     "country": "United States",
     "iata": "SEA",
-    "coordinates": "[47.44900131225586,-122.30899810791016]",
+    "coordinates": [47.44900131225586,-122.30899810791016],
     "price": 150,
     "date": "2017-08-06",
     "days_until": 70
@@ -466,7 +466,7 @@ eqfeed_callback(
     "place": "San Mateo County, California",
     "country": "United States",
     "iata": "SFO",
-    "coordinates": "[37.61899948120117,-122.375]",
+    "coordinates": [37.61899948120117,-122.375],
     "price": 100,
     "date": "2017-08-06",
     "days_until": 70
@@ -476,7 +476,7 @@ eqfeed_callback(
     "place": "Changning-Minhang, Shanghai",
     "country": "China",
     "iata": "SHA",
-    "coordinates": "[31.197900772094727,121.33599853515625]",
+    "coordinates": [31.197900772094727,121.33599853515625],
     "price": 1000,
     "date": "2017-08-06",
     "days_until": 70
@@ -486,7 +486,7 @@ eqfeed_callback(
     "place": "Changi, Singapore",
     "country": "Singapore",
     "iata": "SIN",
-    "coordinates": "[1.35019, 103.994003]",
+    "coordinates": [1.35019, 103.994003],
     "price": 1050,
     "date": "2017-08-06",
     "days_until": 70
@@ -496,7 +496,7 @@ eqfeed_callback(
     "place": "Mascot, Sydney, New South Wales",
     "country": "Australia",
     "iata": "SYD",
-    "coordinates": "[-33.94609832763672,151.177001953125]",
+    "coordinates": [-33.94609832763672,151.177001953125],
     "price": 1000,
     "date": "2017-08-06",
     "days_until": 70
@@ -506,7 +506,7 @@ eqfeed_callback(
     "place": "Bao'an, Shenzhen, Guangdong",
     "country": "China",
     "iata": "SZX",
-    "coordinates": "[22.639299392700195,113.81099700927734]",
+    "coordinates": [22.639299392700195,113.81099700927734],
     "price": 1000,
     "date": "2017-08-06",
     "days_until": 70
@@ -516,7 +516,7 @@ eqfeed_callback(
     "place": "Dayuan, Taoyuan",
     "country": "Taiwan",
     "iata": "TPE",
-    "coordinates": "[25.0777, 121.233002]",
+    "coordinates": [25.0777, 121.233002],
     "price": 600,
     "date": "2017-08-06",
     "days_until": 70
@@ -526,7 +526,7 @@ eqfeed_callback(
     "place": "Mississauga, Ontario",
     "country": "Canada",
     "iata": "YYZ",
-    "coordinates": "[43.6772003174,-79.63059997559999]",
+    "coordinates": [43.6772003174,-79.63059997559999],
     "price": 400,
     "date": "2017-08-06",
     "days_until": 70
@@ -536,7 +536,7 @@ eqfeed_callback(
     "place": "Mar del Plata",
     "country": "Argentina",
     "iata": "MDQ",
-    "coordinates": "[-37.9342,-57.5733]",
+    "coordinates": [-37.9342,-57.5733],
     "price": 1050,
     "date": "2017-07-01",
     "days_until": 1
@@ -546,7 +546,7 @@ eqfeed_callback(
     "place": "Cape Town",
     "country": "South Africa",
     "iata": "CPT",
-    "coordinates": "[-33.9648017883,18.6016998291]",
+    "coordinates": [-33.9648017883,18.6016998291],
     "price": 1030,
     "date": "2017-07-01",
     "days_until": 1
@@ -556,7 +556,7 @@ eqfeed_callback(
     "place": "Lima",
     "country": "Peru",
     "iata": "LIM",
-    "coordinates": "[-12.021900177,-77.1143035889]",
+    "coordinates": [-12.021900177,-77.1143035889],
     "price": 1150,
     "date": "2017-07-01",
     "days_until": 1
